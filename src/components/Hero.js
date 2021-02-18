fefc9e8ad9e31b7afd3ce36029c1e93f0057be89
M ASAD RAFIQUE <itsbrunomars007@gmail.com>
2021-02-18 12:35:31 +0500

    shadow change

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,12 +23,12 @@ const Hero = ({appType, tagLine, description, mainActionText, extraActionText}) 
         </div>
         <div className="relative z-50 flex flex-col items-center justify-between h-48 lg:space-x-8 pt-7 lg:pt-0 lg:flex-row lg:justify-between lg:w-90">
           <button
-            className="pt-3 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all bg-orange-600 rounded-full lg:shadow-2xl lg:ml-5 hover:bg-orange-700 focus:outline-none ring-4 ring-orange-600 lg:ring-2 lg:font-medium "
+            className="pt-3 pb-3 pl-12 pr-12 text-2xl font-semibold text-center text-white transition-all bg-orange-600 rounded-full shadow-none lg:shadow-2xl lg:ml-5 hover:bg-orange-700 focus:outline-none ring-4 ring-orange-600 lg:ring-2 lg:font-medium "
           >
             {mainActionText}
           </button>
           <button
-            className="pt-3 pb-3 text-2xl font-semibold text-center text-orange-500 transition-all rounded-full lg:shadow-2xl lg:mr-5 hover:text-orange-500 hover:bg-gray-50 pl-11 pr-11 bg-gray-50 focus:outline-none ring-4 ring-orange-500 lg:font-medium lg:text-gray-50 lg:bg-opacity-0 lg:ring-2 lg:ring-white"
+            className="pt-3 pb-3 text-2xl font-semibold text-center text-orange-500 transition-all rounded-full shadow-none lg:shadow-2xl lg:mr-5 hover:text-orange-500 hover:bg-gray-50 pl-11 pr-11 bg-gray-50 focus:outline-none ring-4 ring-orange-500 lg:font-medium lg:text-gray-50 lg:bg-opacity-0 lg:ring-2 lg:ring-white"
           >
             {extraActionText}
           </button>
